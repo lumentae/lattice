5180d1517b9530e80e28b81cc490283c971d2855
lumentae <66220444+lumentae@users.noreply.github.com>
2025-11-24 21:47:02 +0100

    Make the ender dragon egg glow when dropped in the end

---
+++ common/src/main/java/dev/lumentae/lattice/mixin/EntityMixin.java
@@ -0,0 +1,28 @@
+package dev.lumentae.lattice.mixin;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+    @Inject(method = "spawnAtLocation(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/phys/Vec3;)Lnet/minecraft/world/entity/item/ItemEntity;", at = @At("RETURN"))
+    public void lattice$spawnAtLocation(ServerLevel level, ItemStack stack, Vec3 offset, CallbackInfoReturnable<ItemEntity> cir) {
+        if (!stack.is(Blocks.DRAGON_EGG.asItem()) && level.dimension().equals(Level.END)) return;
+        setGlowingTag(cir.getReturnValue());
+    }
+
+    @Unique
+    public void setGlowingTag(Entity entity) {
+        entity.setGlowingTag(true);
+    }
+}
